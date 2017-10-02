d81dca68e3ad1b8709c831e8ce3bd1f663db43aa
Brookins Consulting <graham@brookinsconsulting.com>
2017-10-02 15:58:46 -0500

    Added: Added ezinfo and extension.xml extension documentation meta files

---
+++ ezinfo.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * File containing the ezrssfeedInfo class.
+ *
+ * @copyright Copyright (C) 1999 - 2017 Brookins Consulting. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2 (or any later version)
+ * @version //autogentag//
+ * @package ezrssfeed
+ */
+class ezrssfeedInfo
+{
+    static function info()
+    {
+        return array(
+            'Name' => "<a href='http://projects.ez.no/ezrssfeed'>eZ RSS Feed</a>",
+            'Version' => "1.2",
+            'Copyright' => array( "Copyright (C) 1999 - 2017 <a href='http://brookinsconsulting.com'>Brookins Consulting</a>",
+                                  "Copyright (C) 2005 - 2017 <a href='http://ez.no/'>Kristian Hole</a>" ),
+                                  "Copyright (C) 2005 - 2017 <a href='http://ez.no/'>Ole Morten Halvorsen</a>" ),
+            'Author' => "Brookins Consulting, Kristian Hole and Ole Morten Halvorsen",
+            'License' => "GNU General Public License",
+            'info_url' => "http://github.com/brookinsconsulting/ezrssfeed"
+        );
+    }
+}
+?>
